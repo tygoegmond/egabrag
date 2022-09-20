07f253faa5562d8341dc1efeaa2e30954302f609
Tygo Egmond <tygoregmond@gmail.com>
2022-09-20 14:20:44 +0200

    feat: user navigation component

---
+++ resources/views/components/users/navigation.blade.php
@@ -0,0 +1,21 @@
+<div class="w-full bg-white rounded-none border dark:bg-gray-800 dark:border-gray-700">
+    <div class="sm:hidden">
+        <label for="tabs" class="sr-only">Select tab</label>
+        <select id="tabs" class="bg-gray-50 border-0 border-b border-gray-200 text-gray-900 sm:text-sm rounded-t-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <option>Statistics</option>
+            <option>Services</option>
+            <option>FAQ</option>
+        </select>
+    </div>
+    <ul class="hidden text-sm font-medium text-center text-gray-500 rounded-lg divide-x divide-gray-200 sm:flex dark:divide-gray-600 dark:text-gray-400" id="fullWidthTab">
+        <li class="w-full">
+            <button type="button" class="inline-block p-4 w-full text-blue-600 bg-gray-50 rounded-tl-lg hover:bg-gray-100 focus:outline-none dark:bg-gray-700 dark:hover:bg-gray-600">Users</button>
+        </li>
+        <li class="w-full">
+            <button type="button" class="inline-block p-4 w-full bg-gray-50 rounded-tl-lg hover:bg-gray-100 focus:outline-none dark:bg-gray-700 dark:hover:bg-gray-600">Roles</button>
+        </li>
+        <li class="w-full">
+            <button type="button" class="inline-block p-4 w-full bg-gray-50 rounded-tl-lg hover:bg-gray-100 focus:outline-none dark:bg-gray-700 dark:hover:bg-gray-600">Permissions</button>
+        </li>
+    </ul>
+</div>
