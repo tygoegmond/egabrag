5f4f0c20e56e0ed216befa885e3919a8bf2b359f
Tygo Egmond <tygoregmond@gmail.com>
2022-09-28 12:16:19 +0200

    feat: migration and model for post schema

---
+++ app/Models/Post.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'post_category_id',
+        'user_id',
+        'title',
+        'body',
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
+
+    /**
+     * Get the user that owns the post.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the post category that owns the post.
+     */
+    public function postCategory()
+    {
+        return $this->belongsTo(PostCategory::class);
+    }
+}
