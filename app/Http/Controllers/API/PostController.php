549cfb10a6542f245d4d06dd0e22957019cf5b93
Tygo Egmond <tygoregmond@gmail.com>
2022-10-25 13:36:23 +0200

    feat: ability to get posts through API

---
+++ app/Http/Controllers/API/PostController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function getPosts()
+    {
+        $this->authorize('viewAny', Post::class);
+
+        return PostResource::collection(
+            Post::select('id', 'title', 'post_category_id', 'user_id', 'created_at', 'updated_at')->paginate(1)
+        );
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,11 @@ Route::post('/sanctum/token', [AuthController::class, 'token']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware([
+    'auth:sanctum',
+    'verified'
+])->group(function () {
+    Route::get('/posts', [PostController::class, 'getPosts']);
+});
+
